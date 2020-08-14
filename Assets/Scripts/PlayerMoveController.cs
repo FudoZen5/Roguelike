01af03f250de2624ced3b432bd64132dd7695f95
FudoZen5 <fudonakamura@gmail.com>
2020-08-14 11:12:01 +0300

    add move controller

---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMoveController : MonoBehaviour {
+
+	// PUBLIC
+	public SimpleTouchController leftController;
+
+	public float speedMovements = 20f;
+
+	// PRIVATE
+	private Rigidbody _rigidbody;
+
+	void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		// move
+		_rigidbody.AddForce(leftController.GetTouchPosition * speedMovements);
+	}
+
+}
